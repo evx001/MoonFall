ae32f89d009326f346fef78301b9f65b00fa556b
evx001 <eviexeris@yahoo.com>
2014-02-17 13:34:45 -0500

    cabal

---
+++ 2014-0217.hs
@@ -0,0 +1,35 @@
+module Feb17 where 
+
+Code Cabal -- the secret to code supremecy 
+	absolute Beginners encouraged.
+	previous knowledge will only hold you back.
+	we are clearing the slate.
+	we are about using the sharpest tools.  
+	get priceless insight in record time
+	don't spend years or thousands 
+	just getting a clue. 
+	this is not your dad's technology.
+	this not even your brother's. 
+	or your friend's.
+	they are using bronze age tech.
+	this is tempored steel. 
+
+Code Cabal clears away the noise
+	Gives you the short cut to the future.
+	This is not about your next job,
+	This is about your tech life.   
+	Don't wait it will only cost you more, 
+	Or worse you will just choke on the data smog.
+
+This is not easy. 
+This is not just pushing buttons. 
+This is not toy programing. 
+This is defence industry strong.
+
+4 sessions on 4 weekends. 
+Because this is Cabalist Coding 
+there will be no late comers. 
+there will no refunds
+there will be no second chances. 
+there will be no make ups.
+
