c368870e6cbd9c0848655483a33dc012fe9a5e8f
evx001 <eviexeris@gmail.com>
2014-02-18 01:23:19 -0500

    act before

--- 2014-0217.hs
+++ 2014-0217.hs
@@ -1,35 +1,52 @@
 module Feb17 where 
+You want to do computer programming?
+as a career
+or second career
+or you have to manage programmers
+or you have to understand programmers 
 
-Code Cabal -- the secret to code supremecy 
-	absolute Beginners encouraged.
-	previous knowledge will only hold you back.
+Code Cabal is the secret path to code supremecy 
+	absolute Beginners are best situated to take advantage here. 
+	previous knowledge will actually hold you back.
 	we are clearing the slate.
-	we are about using the sharpest tools.  
-	get priceless insight in record time
-	don't spend years or thousands 
-	just getting a clue. 
 	this is not your dad's technology.
-	this not even your brother's. 
+	this not your brother's. 
 	or your friend's.
 	they are using bronze age tech.
-	this is tempored steel. 
-
-Code Cabal clears away the noise
-	Gives you the short cut to the future.
+		
+	don't spend years 
+	or thousands of dollars 
+	to get in at the end of the line. 
+	That's where everybody is.
+	you don't have to be a genius 
+	if you made it to 10th grade Geometry* 
+	if you have a laptop that is no older than 2010
+	has at least 2 gig of ram  
+	running windows 7 or 8 (no vista, no win xp)
+	or a macOS10.7 or later. 
+	you just have to be alert 
+	open to possibility
+	willing to put in focused work 2 - 3 hours daily.
+	(not while watching, tv, netflix, etc)
+	and you can attend 4 sessions on 4 weekends   
+	
+This session of Code Cabal 
+	open you to the shortest route to the summit of code supremacy
 	This is not about your next job,
-	This is about your tech life.   
-	Don't wait it will only cost you more, 
-	Or worse you will just choke on the data smog.
-
+	This is about you tech life.   
+	Don't wait it will only cost you more later. 
 This is not easy. 
-This is not just pushing buttons. 
+This is not  push button development.
 This is not toy programing. 
 This is defence industry strong.
-
-4 sessions on 4 weekends. 
-Because this is Cabalist Coding 
+This is about using the sharpest tools in the draw.  
+It's all in or not in at all.
 there will be no late comers. 
 there will no refunds
-there will be no second chances. 
 there will be no make ups.
+there will be this one shot to jump the competition
 
+Code Cabal 
+			cost 	$353.00 
+but if you act by           	$293.00 
+or if you act by         	$251.00
