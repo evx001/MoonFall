bed8bc4895744167e17c1a1c292d82fcf27856a3
evx001 <eviexeris@gmail.com>
2015-01-20 09:28:53 -0500

    something to try

--- getRandomColor002.swift
+++ getRandomColor002.swift
@@ -1,1 +1,16 @@
-// 
+// PROJECT#000
+// So we cast it to CGFloat.
+ // we get back a UIColor object.
+ // Could try arc4random_uniform method and then MOD it to get a decimal value.
+ func getRandomColor() -> UIColor{
+ 
+        var randomRed:CGFloat = CGFloat(drand48())
+ 
+        var randomGreen:CGFloat = CGFloat(drand48())
+ 
+        var randomBlue:CGFloat = CGFloat(drand48())
+ 
+        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+ 
+    }
+ 
