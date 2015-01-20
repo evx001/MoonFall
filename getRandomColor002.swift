c07db47ab421c6e6530e49be51e123616df42627
evx001 <eviexeris@gmail.com>
2015-01-20 08:55:28 -0500

    update

--- getRandomColor002.swift
+++ getRandomColor002.swift
@@ -1,11 +1,1 @@
- func getRandomColor() -> UIColor{
- 
-        var randomRed:CGFloat = CGFloat(arc4random_uniform())
- 
-        var randomGreen:CGFloat = CGFloat(arc4random_uniform())
- 
-        var randomBlue:CGFloat = CGFloat(arc4random_uniform())
- 
-        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
- 
-    }
+// 
