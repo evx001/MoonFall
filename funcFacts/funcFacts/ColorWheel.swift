dff2bd2248bdb4258f8d823774681a0a57f5d914
evx001 <eviexeris@gmail.com>
2015-01-20 08:13:42 -0500

    random color

---
+++ funcFacts/funcFacts/ColorWheel.swift
@@ -0,0 +1,16 @@
+//
+//  ColorWheel.swift
+//  funcFacts
+//
+//  Created by evx on 1/19/15.
+//  Copyright (c) 2015 evxyz001. All rights reserved.
+//
+
+import Foundation
+import UIKit
+// random color function and we either generate random fractions within a range
+func colorFunc() -> (red:UIColor,green:UIColor,blue:UIColor,1.0){
+    var randColor = UIColor(red: 33/255, green: 34/255, blue: 35/255, alpha: 1.0)
+
+    return (randColor)
+}
