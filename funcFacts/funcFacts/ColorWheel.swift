8389983e77e4128bb14beb8fdc31fc9867229a6c
evx001 <eviexeris@gmail.com>
2015-01-20 09:45:58 -0500

    added to project
    Nothing Broke !!!

--- funcFacts/funcFacts/ColorWheel.swift
+++ funcFacts/funcFacts/ColorWheel.swift
@@ -11,11 +11,11 @@ import UIKit
 // random color function and we either generate random fractions within a range
 func getRandomColor() -> UIColor{
 
-    var randomRed:CGFloat = CGFloat(drand48())
+    var randomRed:CGFloat = CGFloat(UInt32(arc4random_uniform(255)))
 
-    var randomGreen:CGFloat = CGFloat(drand48())
+    var randomGreen:CGFloat = CGFloat(UInt32(arc4random_uniform(255)))
 
-    var randomBlue:CGFloat = CGFloat(drand48())
+    var randomBlue:CGFloat = CGFloat(UInt32(arc4random_uniform(255)))
 
     return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
 
