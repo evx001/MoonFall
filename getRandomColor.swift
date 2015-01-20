c861f4b4955371c34d981479eb452b5ba05e2466
evx001 <eviexeris@gmail.com>
2015-01-20 09:23:19 -0500

    best

--- getRandomColor.swift
+++ getRandomColor.swift
@@ -1,3 +1,6 @@
+ // drand48()  returns a Double value 
+ // whereas the UIColour init method expects the value to be in CGFloat. 
+ // So we cast it to CGFloat.
  func getRandomColor() -> UIColor{
  
         var randomRed:CGFloat = CGFloat(drand48())
@@ -9,3 +12,5 @@
         return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
  
     }
+
+ // DRAND48 Linux OS psuedo-random number generator uses: Linear congruential generator.
