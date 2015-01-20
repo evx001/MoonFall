e34c27f473b4c120f46cec795f9711c9e6cf486f
evx001 <eviexeris@gmail.com>
2015-01-20 09:45:31 -0500

    this could be the ONE

--- getRandomColor002.swift
+++ getRandomColor002.swift
@@ -1,16 +1,16 @@
 // PROJECT#000
-// So we cast it to CGFloat.
+// So we cast it to CGFloat ultimately but UInt32 initially
  // we get back a UIColor object.
- // Could try arc4random_uniform method and then MOD it to get a decimal value.
+ // did try arc4random_uniform method and then MOD it to get a decimal value.
  func getRandomColor() -> UIColor{
- 
-        var randomRed:CGFloat = CGFloat(drand48())
- 
-        var randomGreen:CGFloat = CGFloat(drand48())
- 
-        var randomBlue:CGFloat = CGFloat(drand48())
- 
-        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
- 
-    }
+
+    var randomRed:CGFloat = CGFloat(UInt32(arc4random_uniform(255)))
+
+    var randomGreen:CGFloat = CGFloat(UInt32(arc4random_uniform(255)))
+
+    var randomBlue:CGFloat = CGFloat(UInt32(arc4random_uniform(255)))
+
+    return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+
+}
  
