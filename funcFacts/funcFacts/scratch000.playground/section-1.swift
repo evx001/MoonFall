b590ee34c9611f16348fd3df502fa2c1b55ad2cc
evx001 <eviexeris@gmail.com>
2015-01-20 15:39:22 -0500

    tested drand48

--- funcFacts/funcFacts/scratch000.playground/section-1.swift
+++ funcFacts/funcFacts/scratch000.playground/section-1.swift
@@ -5,11 +5,17 @@ import UIKit
 
 //UIColor 
 
-func xFl ()-> Int{
-var x = arc4random()
-    return ()
-}
-
-
 
 // var redColor = UIColor(red: 144/255, green: 1122/255, blue: 1122/255, alpha: 1)
+
+func getRandomColor() -> UIColor{
+
+    var randomRed:CGFloat = CGFloat(drand48())
+
+    var randomGreen:CGFloat = CGFloat(drand48())
+
+    var randomBlue:CGFloat = CGFloat(drand48())
+
+    return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+    
+}
