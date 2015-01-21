fb72efb949fc231f7e49d42162fcbb0a90400214
evx001 <eviexeris@gmail.com>
2015-01-21 06:31:37 -0500

    stormyWeatherApp

---
+++ stormyWeather/stormyWeather/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  stormyWeather
+//
+//  Created by evx on 1/20/15.
+//  Copyright (c) 2015 Phunctor.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    private let apikey = "6e916559d9447e87ff97ee163ddd4b0a"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
