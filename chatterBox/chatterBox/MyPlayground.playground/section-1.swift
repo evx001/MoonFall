adf1ddcbc76483eb5250742dbf03dbfa7a08c1c5
evx001 <eviexeris@gmail.com>
2015-01-30 17:00:56 -0500

    chatterBox

---
+++ chatterBox/chatterBox/MyPlayground.playground/section-1.swift
@@ -0,0 +1,11 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var myVarInt = 5
+var myString = String(myVarInt)
+var myString_ = "\(myVarInt)"
+
+let myStr = "5"
+let myIntFrom:Int = myStr.toInt()!
+let myD:Double = Double(myStr.toInt()!)
