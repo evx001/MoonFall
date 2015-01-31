04fb8deb33a9939c2fcf30232b1d5ba6b2848389
evx001 <eviexeris@gmail.com>
2015-01-31 10:14:13 -0500

    eh!

---
+++ stormyWeather/nads.hs
@@ -0,0 +1,18 @@
+module Nads where 
+{-
+unit instead of return
+
+★ instead of bind 
+
+------------------------}
+-- Basic actions in IO monad have “side
+-- Reviewing IO Monad
+
+
+
+echo :: IO ()
+actions:
+echo = do { b <- isEOF;
+            if not b then do
+              { x <- getChar; putChar x; echo }
+            else return () }
