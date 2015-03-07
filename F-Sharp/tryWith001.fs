afd25c9d8c761109c634d6b22375dbc82a019b89
evx001 <eviexeris@gmail.com>
2015-03-07 00:07:10 -0500

    try

---
+++ F-Sharp/tryWith001.fs
@@ -0,0 +1,24 @@
+// exceptionalism000
+open System.IO
+
+
+try 
+    use file000 = File.OpenText "FSharpMobileDev.txt"
+    file000.ReadToEnd() |>  printfn "%A" 
+ with 
+ |    :? FileNotFoundException as ex -> 
+      printfn "%A was not Found" ex.FileName 
+ |    _  -> printfn "Error loading file"
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    System.Console.ReadLine() |> ignore
+    0 // return an integer exit code
+ 
+
+
+
+
+
+
