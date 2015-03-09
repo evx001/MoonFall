5db7c0bb148362b43b87ee0aafc856fed385a302
evx001 <eviexeris@gmail.com>
2015-03-09 16:52:09 -0400

    private constructor

---
+++ F-Sharp/constructorPrivate.fs
@@ -0,0 +1,10 @@
+// private constructor
+type Greeter private () = 
+    static let _instance = lazy (Greeter())
+    static member Instance with get() = _instance.Force() 
+    member x.SayHello() = printfn "hello" 
+
+Greeter.Instance.SayHello() 
+////////////////////////
+//> hello
+
