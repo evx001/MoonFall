316d47c87db013e932bbbd8f0d0518d74f938875
evx001 <eviexeris@gmail.com>
2015-03-09 19:09:04 -0400

    fields

---
+++ F-Sharp/typeConstructorAdditional000.fs
@@ -0,0 +1,20 @@
+// fields 
+// let bindings   
+
+
+type Person ()  = 
+    [<DefaultValue>] val mutable n : string 
+    member x.Name
+        with get() = x.n
+        and set(v) = x.n <- v
+
+let p = Person() 
+p.Name;; 
+
+   
+   
+   
+ 
+
+
+
