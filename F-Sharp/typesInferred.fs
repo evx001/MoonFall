e9fb9324a5c962da69e3b4d336984379c68f4228
evx001 <eviexeris@gmail.com>
2015-03-09 05:43:29 -0400

    inferred the types

---
+++ F-Sharp/typesInferred.fs
@@ -0,0 +1,7 @@
+type Person (name, age )  = 
+    do printfn "Creating person: %s (Age: %i)" name age 
+    member x.Name = name 
+    member x.Age = age 
+ let me = Person' ("Dave", 33)
+ /////////////////////////////////////////////////
+ // > Creating person: Dave (Age: 33)
